1afee58291b784ad8ce88972dae05d99958fdcb5
Abu Naser Muhammad Arman <arman.rajbari@gmail.com>
2024-11-02 20:40:45 +0600

    styled mobile navbar and added function

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  document.getElementById("mobile-nav-links-container").classList.toggle("hidden");
+
+  document.getElementById("hamburger-button").classList.toggle("hidden");
+
+  document.getElementById("close-button").classList.toggle("hidden");
+}
