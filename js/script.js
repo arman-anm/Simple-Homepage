040a78afe2eb3b4532776f1eb940406036d2c5ec
Abu Naser Muhammad Arman <arman.rajbari@gmail.com>
2024-11-03 01:44:22 +0600

    added theme toggle function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 function toggleMenu() {
   document.getElementById("hamburger-button").classList.toggle("hidden");
-  
-  document.getElementById("close-button").classList.toggle("hidden");
-  
-  document.getElementById("mobile-nav-links-container").classList.toggle("hidden"); 
 
+  document.getElementById("close-button").classList.toggle("hidden");
+
+  document.getElementById("mobile-nav-links-container").classList.toggle("hidden");
+}
+
+function toggleTheme() {
+  // Check the current theme and toggle between 'light' and 'dark'
+  const currentTheme = document.body.getAttribute("data-theme");
+
+  if (currentTheme === "light") {
+    document.body.setAttribute("data-theme", "dark");
+  } else {
+    document.body.setAttribute("data-theme", "light");
+  }
+
+  // Toggle logo visibility for light and dark themes
+  document.querySelectorAll(".element-for-light-theme").forEach(element => {
+    element.classList.toggle("hidden");
+  });
+
+  document.querySelectorAll(".element-for-dark-theme").forEach(element => {
+    element.classList.toggle("hidden");
+  });
   
 }
