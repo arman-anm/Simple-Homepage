eddacf7724e607a6da5c87011e2e6a5b22000f9e
Abu Naser Muhammad Arman <arman.rajbari@gmail.com>
2024-11-02 23:46:56 +0600

    tweaked the organized codebase

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
-  document.getElementById("mobile-nav-links-container").classList.toggle("hidden");
-
   document.getElementById("hamburger-button").classList.toggle("hidden");
-
+  
   document.getElementById("close-button").classList.toggle("hidden");
+  
+  document.getElementById("mobile-nav-links-container").classList.toggle("hidden"); 
+
+  
 }
